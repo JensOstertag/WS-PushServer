6b8c79c0b2fcb042df38cca34188f841256fe221
JensOstertag <JensOstertag@gmx.de>
2023-10-04 17:28:27 +0200

    Optimize code

--- src/main/java/jensostertag/pushserver/eventhandler/MessageHandler.java
+++ src/main/java/jensostertag/pushserver/eventhandler/MessageHandler.java
@@ -2,6 +2,7 @@ package jensostertag.pushserver.eventhandler;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import jensostertag.pushserver.event.Event;
 import jensostertag.pushserver.event.EventHandler;
 import jensostertag.pushserver.event.EventInitiator;
 import jensostertag.pushserver.event.Listener;
@@ -17,6 +18,7 @@ import jensostertag.pushserver.objects.UnregisteredWebSocketMessage;
 import jensostertag.pushserver.objects.WebSocketChannel;
 import jensostertag.pushserver.objects.WebSocketMessage;
 import jensostertag.pushserver.protocol.MessageCreator;
+import jensostertag.pushserver.protocol.MessageType;
 
 import java.util.UUID;
 
@@ -28,7 +30,7 @@ public class MessageHandler implements Listener {
         try {
             switch (event.getMessageType()) {
                 case ERROR, CLIENT_ACK, CLIENT_PUSH -> {}
-                case CLIENT_SUBSCRIBE -> {
+                case CLIENT_SUBSCRIBE, CLIENT_UNSUBSCRIBE -> {
                     String uuidString = json.get("uuid").getAsString();
                     UUID uuid = UUID.fromString(uuidString);
                     String channel = json.get("data").getAsJsonObject().get("channel").getAsString();
@@ -42,25 +44,13 @@ public class MessageHandler implements Listener {
 
                     Client client = Client.getClient(uuid);
 
-                    ClientSubscribeEvent subscribeEvent = new ClientSubscribeEvent(client, webSocketChannel);
-                    EventInitiator.trigger(subscribeEvent);
-                }
-                case CLIENT_UNSUBSCRIBE -> {
-                    String uuidString = json.get("uuid").getAsString();
-                    UUID uuid = UUID.fromString(uuidString);
-                    String channel = json.get("data").getAsJsonObject().get("channel").getAsString();
-                    WebSocketChannel webSocketChannel = WebSocketChannel.getWebSocketChannel(channel);
-
-                    if(!PermissionHandler.hasPermission(uuid, event.getWebSocket())) {
-                        String jsonMessage = MessageCreator.error(403, "Forbidden", "The given UUID does not belong to this WebSocket connection");
-                        WebSocketMessage webSocketMessage = new UnregisteredWebSocketMessage(event.getWebSocket(), jsonMessage);
-                        WebSocketMessageQueue.getInstance().queueMessage(webSocketMessage);
+                    Event delegateEvent;
+                    if(event.getMessageType() == MessageType.CLIENT_SUBSCRIBE) {
+                        delegateEvent = new ClientSubscribeEvent(client, webSocketChannel);
+                    } else {
+                        delegateEvent = new ClientUnsubscribeEvent(client, webSocketChannel);
                     }
-
-                    Client client = Client.getClient(uuid);
-
-                    ClientUnsubscribeEvent subscribeEvent = new ClientUnsubscribeEvent(client, webSocketChannel);
-                    EventInitiator.trigger(subscribeEvent);
+                    EventInitiator.trigger(delegateEvent);
                 }
             }
         } catch(ClientNotFoundException e) {
